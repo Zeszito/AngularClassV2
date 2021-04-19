f0107ff693e660d2cc61bf875c570fa0f32bc285
Zeszito <liricractos@gmail.com>
2021-04-19 10:51:29 +0100

    calculadora added

--- aulasV2/src/app/app-routing.module.ts
+++ aulasV2/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: HomeComponent},
+  {path:"home", component: HomeComponent},
+  {path:"calculadora", component: CalculatorComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- aulasV2/src/app/app.module.ts
+++ aulasV2/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { MainNavigationComponent } from './components/main-navigation/main-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CalculatorComponent,
+    MainNavigationComponent
   ],
   imports: [
     BrowserModule,
